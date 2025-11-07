b5cf5fa3fb5ab258c929e9bd02a32b67196959dd
Nicolò Rinaldi <nicolo.rinaldi99@gmail.com>
2025-11-07 14:57:11 +0100

    typos

--- src/main/java/io/nicolorinaldi/Main.java
+++ src/main/java/io/nicolorinaldi/Main.java
@@ -1,4 +1,4 @@
-package io.nicolo-rinaldi;
+package io.nicolorinaldi;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
